ca116c62c180f6b60adf8fc72a9e3f3d763acb51
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-11-30 00:09:32 -0300

    feat: created and configured producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
 	fmt.Println("Hello Go")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers":   "gokafka_kafka_1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
 }
