21e0276dce96ada28c7bacc7a2d14a318f855a41
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-11-30 23:19:01 -0300

    feat: published first message

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	fmt.Println("Hello Go")
+	producer := NewKafkaProducer()
+	Publish("transferiu", "teste", producer, []byte("transferecia2"), deliveryChan)
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers":   "gokafka_kafka_1:9092",
-		"delivery.timeout.ms": "0",
-		"acks":                "all",
-		"enable.idempotence":  "true",
+		"bootstrap.servers": "gokafka_kafka_1:9092",
 	}
 	p, err := kafka.NewProducer(configMap)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, deliveryChan)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ricardomaricato/gokafka
 
-go 1.16
+go 1.17
+
+require github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
