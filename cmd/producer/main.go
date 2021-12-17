257b5e327a3ce7af797072fa8e141f2017b6b09e
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-12-16 23:55:13 -0300

    feat: important parameters

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -29,7 +29,10 @@ func main() {
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": "gokafka_kafka_1:9092",
+		"bootstrap.servers":   "gokafka_kafka_1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
 	}
 	p, err := kafka.NewProducer(configMap)
 	if err != nil {
